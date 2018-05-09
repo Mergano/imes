b990236ed1802dabba162a2260e4c9c39f85fcfb
jukbot <chukkrit.juk@gmail.com>
2018-05-09 19:00:47 +0700

    fix sw generator

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{jpg,svg,png,html,json,md,js,css}"
+  ],
+  "swDest": "/Users/Juk/OneDrive/Projects/imes-website/sw.js"
+};
